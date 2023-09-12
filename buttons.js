fb337f795c415d9ce93588fbece84714d5a5f7f8
imgnation1 <64916062+imgnation1@users.noreply.github.com>
2023-09-12 22:39:17 +0300

    Add files via upload

---
+++ buttons.js
@@ -0,0 +1,39 @@
+let activeIndex = 0;
+
+const groups = document.getElementsByClassName("card-group");
+
+const handleLoveClick = () => {
+  // Bump active index
+  console.log("ok");
+  const nextIndex = activeIndex + 1 <= groups.length - 1 ? activeIndex + 1 : 0;
+  const currentGroup = document.querySelector(`[data-index="${activeIndex}"]`),
+    nextGroup = document.querySelector(`[data-index="${nextIndex}"]`);
+
+  // Active group becomes after
+  currentGroup.dataset.status = "after";
+  // next group becomes active
+
+  nextGroup.dataset.status = "becoming-active-from-before";
+
+  setTimeout(() => {
+    nextGroup.dataset.status = "active";
+    activeIndex = nextIndex;
+  });
+};
+const handleHateClick = () => {
+  // Bump active index
+  const nextIndex = activeIndex - 1 >= 0 ? activeIndex - 1 : groups.length - 1;
+  const currentGroup = document.querySelector(`[data-index="${activeIndex}"]`),
+    nextGroup = document.querySelector(`[data-index="${nextIndex}"]`);
+
+  // Active group becomes after
+  currentGroup.dataset.status = "before";
+  // next group becomes active
+
+  nextGroup.dataset.status = "becoming-active-from-after";
+
+  setTimeout(() => {
+    nextGroup.dataset.status = "active";
+    activeIndex = nextIndex;
+  });
+};
